2f4bc2460a5c4cd70d8f4ff719743e8ef55f2e58
Jennifer M <156385936+JenniferM04@users.noreply.github.com>
2024-04-09 12:11:07 -0400

    Final

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,7 @@
 const { response } = require("express");
+const { model } = require('mongoose');
 const bcrypt = require('bcrypt');
+const saltRounds = 12;
 
 const { User } = require("../models/user");
 
@@ -9,9 +11,13 @@ const { User } = require("../models/user");
  * @returns 
  */
 function userExists(usernameToFind) {
-    // HINT use .find() with query selection
-    console.log('--TODO: implement userExists')
-}
+    User.findOne({username: usernameToFind}).then(function(user){
+        return user;
+    })
+    .catch((err) => {
+        return null;
+    });
+};
 
 /**
  * renders home view
@@ -22,7 +28,7 @@ exports.homeView = (req, res) => {
     res.render('home', {
         pageTitle: 'INFT 2202 - Home Page',
     })
-}
+};
 
 /**
  * render the login page
@@ -35,7 +41,7 @@ exports.getLogin = (req, res, next) => {
         pageTitle: 'Login',
         errorMessage: ''
     });
-}
+};
 
 /**
  * render the login failure page
@@ -43,12 +49,12 @@ exports.getLogin = (req, res, next) => {
  * @param {*} res 
  * @param {*} next 
  */
-exports.getLoginFailure = (req, res, next) => {
+const getLoginFailure = (req, res, next) => {
     res.render('login', {
         pageTitle: 'Login',
         errorMessage: 'Username/password combination does not exist. Please try again.'
     });
-}
+};
 
 /**
  * render the login success page
@@ -56,12 +62,12 @@ exports.getLoginFailure = (req, res, next) => {
  * @param {*} res 
  * @param {*} next 
  */
-exports.getLoginSuccess = (req, res, next) => {
+const getLoginSuccess = (req, res, next) => {
     res.render('login-success', {
         pageTitle: '',
         user: { username: req.body.username }
     });
-}
+};
 
 /**
  * handle login form submit
@@ -70,21 +76,36 @@ exports.getLoginSuccess = (req, res, next) => {
  * @param {*} next 
  */
 exports.postLogin = (req, res) => {
-    let username = req.body.username;
-    let password = req.body.password;
+    let usernameEntry = req.body.username;
+    let passwordEntry = req.body.password;
     // check to see if user pass combo exists
     // render either login-failure or login-success
-    // TODO: check against DB instead of hardcoded values
+    // check against DB instead of hardcoded values
+    User.findOne({"username": usernameEntry}).then(function(user){
+        if(user){
+            // username match
+            // check password hash
+            bcrypt.compare(passwordEntry, user.hashPassword, function(err, result) {
+                if (err == null && result) {
+                    // correct password
+                    getLoginSuccess(req, res);
+                } else{
+                    // either an error or incorrect password entry
+                    getLoginFailure(req, res);
+                }
+            });
+        } else{
+            // user not found
+            //show error
+            getLoginFailure(req, res);
+        }
 
-    if (username === 'admin' && password === 'admin') {
-        // user password combination matches, show success
-        this.getLoginSuccess(req, res);
-    } else {
-        // user password combination failed/does not exist
-        // show error message
-        this.getLoginFailure(req, res);
-    }
-}
+        //if(userList?.length > 0){
+            // user exists with that username
+            //bcrypt.compare(passwordEntry, )
+        //}
+    });
+};
 
 
 /**
@@ -98,7 +119,7 @@ exports.getRegister = (req, res, next) => {
         pageTitle: 'Register a New Account',
         errorMessage: ''
     });
-}
+};
 
 /**
  * handle register form submit
@@ -107,20 +128,51 @@ exports.getRegister = (req, res, next) => {
  * @param {*} next 
  */
 exports.postRegister = (req, res) => {
-    let username = req.body.username;
-    let password = req.body.password;
+    let username = req.body.username.trim();
+    let password = req.body.password.trim();
+    let errorMessage = ''
 
-    if (userExists(username)) { 
-        // there is already a user with the username
-        // show error message
-        res.render('register', {
-            pageTitle: 'Register a New Account',
-            errorMessage: 'Username is already in use. Please choose another.'
-        }) ;
-    } else {
-        // create a new user, show registerConfirm page
-    }
-}
+    User.findOne({username: usernameEntry})
+    .then(function(user){
+        if (user) {
+            // user exists
+            res.render("register", {
+                pageTitle: "Register a New Account",
+                errorMessage: "Username is already in use. Please choose another."
+            });
+        } else {
+            // user does not exist, create new record.
+
+            // hash password before adding
+            bcrypt.hash(passwordEntry, saltRounds, function(err, hash) {
+                if (err == null && hash){
+                    // no error, hash successful
+
+                    // create user object
+                    let userData = {
+                        username: usernameEntry,
+                        hashPassword: hash
+                    }
+
+                    // creates schema model object
+                    let newUser = new User(userData);
+                    //save new user
+                    newUser.save();
+                    res.render('login', {
+                        pageTitle: 'Login',
+                        errorMessage: ""
+                    });
+                } else {
+                    console.log(err)
+                }
+            });
+        }
+    })
+    .catch((err) => {
+        console.log('An error occured', err);
+    });
+
+};
 
 
 
